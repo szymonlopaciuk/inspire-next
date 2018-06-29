e89c2e173f09bdb649c044ac9d42a449beaa8d4f
Antonio Cesarano <cesarano2607@gmail.com>
2018-06-29 17:37:53 +0200

    suggestions: upgrade to es5
    * INCOMPATIBLE: this breaks the backwards compatibility with ES
    
    Signed-Off-By: Antonio Cesarano <cesarano2607@gmail.com>

--- inspirehep/config.py
+++ inspirehep/config.py
@@ -935,7 +935,6 @@ RECORDS_REST_FACETS = {
     "records-authors": {
         "filters": {
             "arxiv_categories": terms_filter('facet_arxiv_categories'),
-            "inspire_categories": terms_filter('facet_inspire_categories'),
             "institution": terms_filter('facet_institution_name')
         },
         "aggs": {
@@ -956,7 +955,7 @@ RECORDS_REST_FACETS = {
     "records-conferences": {
         "filters": {
             "series": terms_filter('series'),
-            "inspire_categories": terms_filter('inspire_categories.term')
+            "inspire_categories": terms_filter('facet_inspire_categories'),
         },
         "aggs": {
             "series": {
@@ -967,7 +966,7 @@ RECORDS_REST_FACETS = {
             },
             "inspire_categories": {
                 "terms": {
-                    "field": "inspire_categories.term",
+                    "field": "facet_inspire_categories",
                     "size": 20
                 }
             },
@@ -975,6 +974,7 @@ RECORDS_REST_FACETS = {
                 "date_histogram": {
                     "field": "opening_date",
                     "interval": "year",
+                    "format": "yyyy",
                     "min_doc_count": 1,
                     "order": {"_count": "desc"}
                 }
